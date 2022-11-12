b940755777d693626c512cc11f75d188ef8fdaff
MindTheGapM8 <nictermote2000@gmail.com>
2022-11-12 16:47:02 +0100

    Added data and initial data treatment

---
+++ utils/genres.py
@@ -0,0 +1,86 @@
+genre_wordsets = dict([
+    ('Silent', 
+    ['silent', 'goat']),
+
+    ('B & W', 
+    ['black-and-white']),
+
+    ('Indie', 
+    ['indie', 'short', 'experimental', 'art']),
+
+    ('Melodrama', 
+    ['melodrama']),
+
+    ('Animation', 
+    ['animation', 'motion', 'cartoon']),
+
+    ('Action', 
+    ['action', 'combat', 'martial', 'boxing', 'sword']),
+
+    ('Adventure', 
+    ['adventure', 'swashbuckle', 'swashbuckler', 'travel']),
+
+    ('Biography', 
+    ['biography', 'biographical', 'biopic']),
+
+    ('Comedy', 
+    ['comedy', 'slapstick', 'satire', 'parody', 'tragicomedy', 'stoner', 'comedies']),
+
+    ('Crime', 
+    ['crime', 'detective', 'spy', 'noir', 'criminal', 'prison', 'slasher', 'gangster', 'whodunit', 'caper', 'heist', 'prison']),
+
+    ('Drama', 
+    ['drama', 'tragicomedy', 'americana', 'tragedy']),
+
+    ('Documentary', 
+    ['documentary', 'anthropology', 'archives', 'environmental']),
+
+    ('Fantasy', 
+    ['fantasy', 'dystopia', 'fairy', 'apocalyptic', 'zombie', 'monster', 'superhero', 'supernatural', 'werewolf', 'steampunk', 'alien']),
+
+    ('Family', 
+    ['family', 'children', 'christmas', 'school', 'holiday']),
+
+    ('Historical', 
+    ['historical', 'history', 'period']),
+
+    ('Horror', 
+    ['horror', 'haunted', 'splatter']),
+
+    ('Musical', 
+    ['musical', 'music', 'operetta', 'singing', 'opera', 'concert']),
+
+    ('Mystery', 
+    ['mystery',]),
+
+    ('Romance', 
+    ['romance', 'romantic']),
+
+    ('Science Fiction', 
+    ['time', 'sci']),
+
+    ('Sports', 
+    ['sports',]),
+
+    ('Thriller', 
+    ['thriller', 'psychological', 'suspense']),
+
+    ('War', 
+    ['war']),
+
+    ('Western', 
+    ['western', 'cowboy']),
+
+    ('Propaganda', 
+    ['propaganda', 'patriotic']),
+
+    ('Erotic', 
+    ['sex', 'adult', 'pornographic', 'sexploitation', 'pornography']),
+
+    ('Diversity', 
+    ['gay', 'lgbt', 'race', 'black', 'women', 'feminist', 'disability', 'disabilities']),
+
+    ('Religious', 
+    ['Hagiography', 'christian', 'cult', 'religious']),
+
+])
